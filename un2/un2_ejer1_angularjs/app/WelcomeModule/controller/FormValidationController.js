c9e36f3f058b9f4347e5dfe053ef3b9c06eea166
Excoriate <atr.engineer@outlook.com>
2016-05-11 19:53:07 -0300

    Cambios relativos a la primera aplicación en AngularJs.

--- un2/un2_ejer1_angularjs/app/WelcomeModule/controller/FormValidationController.js
+++ un2/un2_ejer1_angularjs/app/WelcomeModule/controller/FormValidationController.js
@@ -7,8 +7,22 @@ var app = angular.module('LoginFormModule',
 []);
 
 
-//Controlle.
+//Controller.
 app.controller('LoginFormController', function ($scope, $http){
+    // Modelo, entidad generada para sustentar la vista.
+    $scope.formModel = {};
+
+    //function que llama al Submit.
+    $scope.onSubmit = function(){
+     console.log("Llegue, ¡primer submit con AngularJs");
+        console.log($scope.formModel);
+
+        //más adelante, haremos llamadas HTTP.
+
+
+    };
+
+
 
 
 }) ;
